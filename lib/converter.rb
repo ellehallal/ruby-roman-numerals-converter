e8cf5f0c01dba48b7a0da23c83e4275079f5f6a6
itsellej <daniellehallal@gmail.com>
2018-11-02 10:19:23 +0000

    Test passed: converts 3 to 'III'. Refactored convert method

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,13 +1,28 @@
 class RomanNumerals
-  def convert(arabic)
-    # if arabic == 1
-    #   "I"
-    # elsif arabic == 2
-    #   "II"
-    # else
-    #   ""
-    # end
-    "I" * arabic
+  def convert(number)
+  # roman = ["III", "II", "I"]
+  # numbers = [3, 2, 1]
+    final = []
+    roman = {
+      3 => "III",
+      2 => "II",
+      1 => "I"
+    }
+
+    roman.each do |key, value|
+      p number
+      p key
+      p value
+
+      while number >= key
+         final << value
+         number =- key
+         p number
+      end
+      p number
+    end
+
+    final.join("")
   end
 
 end
