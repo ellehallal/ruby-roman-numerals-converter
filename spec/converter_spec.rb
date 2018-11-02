8a4d3982116212c068ff16c7ee60d997b01ca70a
itsellej <daniellehallal@gmail.com>
2018-11-02 10:42:24 +0000

    Passing test: converts num in hash to a roman numeral. Add all key roman numerals to roman hash in converter method

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -28,4 +28,32 @@ RSpec.describe RomanNumerals do #testing converter class
     roman_numerals = RomanNumerals.new
     expect(roman_numerals.convert(4)).to eq('IV')
   end
+
+  roman = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    8 => "VIII",
+    7 => "VII",
+    6 => "VI",
+    5 => "V",
+    4 => "IV",
+    3 => "III",
+    2 => "II",
+    1 => "I"
+  }
+
+  roman.each do |num, symbol|
+    it 'converts num to a romal numeral' do
+      roman_numerals = RomanNumerals.new
+      expect(roman_numerals.convert(num)).to eq(symbol)
+    end
+  end
 end
